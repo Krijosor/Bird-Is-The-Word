0179214e25f344760505daef9de3564a51b39518
havardhall <hav.hallerud@gmail.com>
2025-07-20 21:31:50 +0200

    streamlined code for __getitem__. Now the image preprocessing is mostly numpy -> tensor instead of back and forth

--- src/BWDatasets.py
+++ src/BWDatasets.py
@@ -49,27 +49,31 @@ class TrainDataSet(Dataset):
         label = self.labels[idx]
         bb_path = self.bb_paths[idx]
         image = Image.open(self.img_paths[idx]).convert('RGB')
+        # image = cv2.imread(self.img_paths[idx], flags=cv2.IMREAD_COLOR_BGR)
+        image = np.array(image)
+        image = image[:, :, ::-1].copy()
 
         # Make sure that the image is a tensor, and not PIL
-        if not (isinstance(image, torch.Tensor)):
-            image = T.ToTensor()(image)
+        # if not (isinstance(image, torch.Tensor)):
+        #     image = T.ToTensor()(image)
         
         bb = _calculate_bb_cords(image=image, bb=_bb_txt_to_list(bb_path=bb_path)) # Retrieve bb cords directly before returning
         image = _crop_image_with_bb(image, bb)
 
-        image = T.Normalize(           # ImageNet stats
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225],
-        )(image)
+
+        # image = T.Normalize(           # ImageNet stats
+        #     mean=[0.485, 0.456, 0.406],
+        #     std=[0.229, 0.224, 0.225],
+        # )(image)
 
         # Cast to numpy and change to integers
-        image = tensor_to_numpy(image)
-        image = (image * 255).clip(0, 255).astype('uint8')
+        # image = tensor_to_numpy(image)
+        # image = (image * 255).clip(0, 255).astype('uint8')
 
         # Convert image to OpenCV BGR format, supersample, and convert back to RGB 
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        image = self.upres.upsample(image)
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        #image = self.upres.upsample(image)
+
 
         # TODO: Deskew bildet
 
@@ -82,9 +86,11 @@ class TrainDataSet(Dataset):
         # TODO: Binarize / threshold bildet -> en svart og hvit maske
 
         # TODO: Morphological cleanup -> fyll inn manglende deler av bokstaver og fjern flekker
-
         
+
         # TODO: Data augmentation
+
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         # Make sure that the image is a tensor
         if not (isinstance(image, torch.Tensor)):
@@ -97,7 +103,6 @@ class TrainDataSet(Dataset):
         # Convert image back to ints
         image = convert_dtype(image)
 
-        #check_image_state(image, "after all steps")
         return image, bb, label
 
 '''
@@ -110,22 +115,6 @@ We set copy = False since we won't use the pre-manipulated image again.
 def convert_dtype(image:torch.Tensor) -> torch.Tensor:
     image = (image.clamp(0.0, 1.0) * 255).byte()
     return image
-
-# def convert_dtype(image:torch.Tensor):
-#     if image.dtype == 'uint8':
-#         # image = image.astype(np.float32, copy=False) / 255.0
-#         image = (image / 255).to(dtype=torch.float32)
-#         image[image > 1.0] = 1.0
-#         image[image < 0.0] = 0.0
-
-#     elif image.dtype == 'float32':
-#         # image *= 255
-#         # image = image.astype(np.uint8, copy=False)
-#         image = (image * 255).to(dtype=torch.uint8)
-#         image[image> 255] = 255
-#         image[image < 0] = 0
-
-#     return image
 
 '''
 Converts a tensor containing a PIL image into a numpy array.
@@ -142,7 +131,7 @@ def tensor_to_numpy(img_tensor:torch.Tensor):
         return img_np.permute(1, 2, 0).numpy()
     
     return img_np.numpy()
-    
+
 '''
 Opens a single image so that it can easily be converted into
 a format that can go into the PaddleOCR model
@@ -154,20 +143,6 @@ def open_image(filepath):
     # TODO: Add preprocessing steps
     image = tensor_to_numpy(image)
     return image
-
-'''
-Crops an image to the bounding box that is provided
--------- Helper function --------
-'''
-def _crop_image_with_bb(image, bb):
-    x = int(bb[0])
-    y = int(bb[1])
-    width = int(bb[2]) - int(bb[0])
-    height = int(bb[3]) - int(bb[1])
-
-    img_crop = F.crop(image, top=y, left=x, height=height, width=width)
-    return img_crop
-
 
 '''
 Converts a txt file containing the bounding boxes to a ring
@@ -182,6 +157,37 @@ def _bb_txt_to_list(bb_path):
     
     return bb
 
+
+'''
+Crops an image to the bounding box that is provided
+-------- Helper function --------
+'''
+def _crop_image_with_bb(image, bb):
+
+    x, y, x2, y2 = map(int, bb)
+
+    # x = int(bb[0])
+    # y = int(bb[1])
+    # x2 = int(bb[2])
+    # y2  = int(bb[3])
+    # width = int(bb[2]) - int(bb[0])
+    # height = int(bb[3]) - int(bb[1])
+
+    # x2 = x + width
+    # y2 = y + height
+
+    # x, y, x2, y2 = map(int, bb)
+    return image[y:y2, x:x2, :]
+
+# def _crop_image_with_bb(image, bb):
+#     x = int(bb[0])
+#     y = int(bb[1])
+#     width = int(bb[2]) - int(bb[0])
+#     height = int(bb[3]) - int(bb[1])
+
+#     img_crop = F.crop(image, top=y, left=x, height=height, width=width)
+#     return img_crop
+
 '''
 Calculates the Bounding box coordinates for an image
 @param image - the image's dimensions is needed for box calculations
@@ -190,22 +196,21 @@ Calculates the Bounding box coordinates for an image
 '''
 def _calculate_bb_cords(image, bb):
 
-    img_w = image.shape[2]
-    img_h = image.shape[1]
+    img_H, img_W, _ = image.shape
 
     # Calculate x and y coordinates of the bb
-    bb_x = img_w * float(bb[1])
-    bb_y = img_h * float(bb[2])
+    bb_x = img_W * float(bb[1])
+    bb_y = img_H * float(bb[2])
 
     # Calculate height and width of the bb, and divide by 2
-    bb_w = (image.shape[2] * float(bb[3])) / 2
-    bb_h = (image.shape[1] * float(bb[4])) / 2
+    bb_w = (img_W * float(bb[3])) / 2
+    bb_h = (img_H * float(bb[4])) / 2
 
     # Calculate corners of bb
     min_x = max(0, bb_x - (bb_w))
-    max_x = min(img_w, bb_x + (bb_w))
+    max_x = min(img_W, bb_x + (bb_w))
     min_y = max(0, bb_y - (bb_h))
-    max_y = min(img_h, bb_y + (bb_h))
+    max_y = min(img_H, bb_y + (bb_h))
 
     return min_x, min_y, max_x, max_y
 
@@ -226,6 +231,8 @@ Used for testing, mainly that the preprocessing steps keep the desired output fo
 
 '''
 if __name__ == "__main__":
+    #print(cv2.haveImageReader(".jpg")) 
+    
     label_path = "dataset/datasets/rf/ringcodes.csv"
     image_path = "dataset/datasets/rf/images"
     bb_path = "dataset/datasets/rf/labels"
@@ -234,7 +241,7 @@ if __name__ == "__main__":
     HEIGHT = 224
 
     transform = T.Compose([
-        T.Resize((HEIGHT,WIDTH)),
+        T.Resize((HEIGHT,WIDTH), antialias=True),
         T.Lambda(lambda x: F.rotate(x, 270, expand=True))
         
     ])
